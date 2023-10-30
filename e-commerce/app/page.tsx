52d55e9a32b3bf1596b8b5365b58b36f965d93b5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-30 10:49:48 +0200

    Update page.tsx

--- e-commerce/app/page.tsx
+++ e-commerce/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
+import Container from './components/Container'
+import HomeBanner from './components/HomeBanner'
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className='p-8'>
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+      </Container>
     </div>
   )
 }
