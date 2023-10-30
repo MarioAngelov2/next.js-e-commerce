3ebed619630df2bcd7a275666b9a761c9b2d5f29
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-30 20:43:47 +0200

    Add rating

--- e-commerce/app/product/[productId]/page.tsx
+++ e-commerce/app/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { product } from "@/utils/product";
 import Container from "@/app/components/Container";
 import React from "react";
 import ProductDetails from "./ProductDetails";
+import ListRating from "./ListRating";
 
 interface Params {
     productId?: string;
@@ -12,6 +13,10 @@ const ProductId = ({ params }: { params: Params }) => {
         <div className="p-8">
             <Container>
                 <ProductDetails product={product}/>
+                <div className="flex flex-col mt-20 gap-4">
+                    <div>Add Rating</div>
+                    <ListRating product={product}/>
+                </div>
             </Container>
         </div>
     );
