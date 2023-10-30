d5f65558d71397dd84d5e4cfac54f398ac4e9777
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-30 11:24:19 +0200

    Render product card & pass props to it

--- e-commerce/app/page.tsx
+++ e-commerce/app/page.tsx
@@ -1,15 +1,25 @@
-import Image from 'next/image'
-import Container from './components/Container'
-import HomeBanner from './components/HomeBanner'
+import Image from "next/image";
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
+import { products } from "@/utils/products";
+import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
-  return (
-    <div className='p-8'>
-      <Container>
-        <div>
-          <HomeBanner />
+    return (
+        <div className="p-8">
+            <Container>
+                <div>
+                    <HomeBanner />
+                </div>
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+                    {products.map((product: any) => (
+                        <div key={product.id}>
+                        <ProductCard data={product}/>
+                        </div>
+                    ))}
+                </div>
+            </Container>
         </div>
-      </Container>
-    </div>
-  )
+    );
 }
