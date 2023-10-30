70d4ef69cc576a2e3cc8a4b84d6d2045e3fd1d89
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-30 12:19:29 +0200

    Navigate to product's page

--- e-commerce/app/components/products/ProductCard.tsx
+++ e-commerce/app/components/products/ProductCard.tsx
@@ -5,18 +5,21 @@ import { truncateText } from "@/utils/truncateText";
 import Image from "next/image";
 import React from "react";
 import { Rating } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
     data: any;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+    const router = useRouter();
     const productRating =
         data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
         data.reviews.length;
 
     return (
         <div
+            onClick={() => router.push(`/product/${data.id}`)}
             className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50
         rounded-sm p-2 transition duration-300 hover:scale-105 text-center text-sm"
         >
