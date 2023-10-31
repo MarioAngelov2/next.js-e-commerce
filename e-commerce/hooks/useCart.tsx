69bb12db6635d6b0fb8d170aa7977510abceddcc
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-31 11:19:49 +0200

    Add a product to cart

--- e-commerce/hooks/useCart.tsx
+++ e-commerce/hooks/useCart.tsx
@@ -1,7 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { CartProductType } from "@/types/product";
+import { createContext, useCallback, useContext, useState } from "react";
 
 type CartContextType = {
     cartTotalQuantity: number;
+    cartProducts: CartProductType[] | null;
+    handleAddProductToCart: (product: CartProductType) => void;
 };
 
 interface Props {
@@ -12,9 +15,28 @@ export const CartContext = createContext<CartContextType | null>(null);
 
 export const CartContextProvider = (props: Props) => {
     const [cartTotalQuantity, setCartTotalQuantity] = useState(0);
+    const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(
+        null
+    );
+
+    const handleAddProductToCart = useCallback((product: CartProductType) => {
+        setCartProducts((prev) => {
+            let updatedCart;
+
+            if (prev) {
+                updatedCart = [...prev, product];
+            } else {
+                updatedCart = [product];
+            }
+
+            return updatedCart;
+        });
+    }, []);
 
     const value = {
         cartTotalQuantity,
+        cartProducts,
+        handleAddProductToCart,
     };
 
     return (
