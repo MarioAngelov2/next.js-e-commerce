67e22e1019789d03e4e2951d33b8559146ceed21
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-31 09:04:28 +0200

    Update next.config.js

--- e-commerce/next.config.js
+++ e-commerce/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['firebasestorage.googleapis.com']
-    }
-}
+        domains: [
+            "firebasestorage.googleapis.com",
+            "lh3.googleusercontent.com",
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
