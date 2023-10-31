92f202aef0cd5dc08980e66de25f7fed3acbd4aa
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-31 11:26:32 +0200

    Set added product to local storage

--- e-commerce/hooks/useCart.tsx
+++ e-commerce/hooks/useCart.tsx
@@ -1,5 +1,11 @@
 import { CartProductType } from "@/types/product";
-import { createContext, useCallback, useContext, useState } from "react";
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
 
 type CartContextType = {
     cartTotalQuantity: number;
@@ -19,6 +25,13 @@ export const CartContextProvider = (props: Props) => {
         null
     );
 
+    useEffect(() => {
+        const cartItems: any = localStorage.getItem("eShopCartItems");
+        const parsedProducts: CartProductType[] | null = JSON.parse(cartItems);
+
+        setCartProducts(parsedProducts);
+    }, []);
+
     const handleAddProductToCart = useCallback((product: CartProductType) => {
         setCartProducts((prev) => {
             let updatedCart;
@@ -29,6 +42,7 @@ export const CartContextProvider = (props: Props) => {
                 updatedCart = [product];
             }
 
+            localStorage.setItem("eShopCartItems", JSON.stringify(updatedCart));
             return updatedCart;
         });
     }, []);
