9752d326c9680b207ecbe217006e2b3fba728ca0
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-01 18:11:18 +0200

    Create register form

---
+++ e-commerce/app/register/RegisterForm.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+import Heading from "../components/Heading";
+import Input from "../components/inputs/Input";
+import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import Button from "../components/Button";
+import Link from "next/link";
+import { AiOutlineGoogle } from "react-icons/ai";
+
+const RegisterForm = () => {
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+    };
+
+    return (
+        <>
+            <Heading title="Sign Up" />
+            <Button label="Sign up with Google" outline onClick={() => {}} icon={AiOutlineGoogle} />
+            <hr className="bg-slate-300 w-full h-px" />
+            <Input
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="password"
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+                type="password"
+            />
+            <Button
+                label={isLoading ? "Loading" : "Sign Up"}
+                onClick={handleSubmit(onSubmit)}
+            />
+            <p className="text-sm">
+                Already have an accound?
+                <Link className="underline" href="/login">
+                    {" "}
+                    Login
+                </Link>
+            </p>
+        </>
+    );
+};
+
+export default RegisterForm;
