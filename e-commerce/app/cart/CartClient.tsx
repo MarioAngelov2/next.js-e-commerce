54e14f79803fcf1de1d5dac2d49bd46a44ad65e7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-01 10:32:35 +0200

    Update CartClient.tsx

--- e-commerce/app/cart/CartClient.tsx
+++ e-commerce/app/cart/CartClient.tsx
@@ -7,9 +7,10 @@ import { MdArrowBack } from "react-icons/md";
 import Heading from "../components/Heading";
 import Button from "../components/Button";
 import ProductContent from "./ProductContent";
+import { formatPrice } from "@/utils/formatPrice";
 
 const CartClient = () => {
-    const { cartProducts, handleClearCart } = useCart();
+    const { cartProducts, handleClearCart, cartTotalAmout } = useCart();
 
     if (!cartProducts || cartProducts.length === 0) {
         return (
@@ -53,7 +54,7 @@ const CartClient = () => {
                 <div className="text-sm flex flex-col gap-1 items-start">
                     <div className="flex justify-between w-full text-base font-semibold">
                         <span>Subtotal</span>
-                        <span>$1.000</span>
+                        <span>{formatPrice(cartTotalAmout)}</span>
                     </div>
                     <p className="text-slate-500">
                         Taxes and shipping calculate at checkout
