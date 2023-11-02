7b88ff6b5fcfe3b87d8d1900014d8f7d1e76e5d3
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-02 16:35:14 +0200

    Get the current user data pass it to child enhancer

--- e-commerce/app/components/nav/Navbar.tsx
+++ e-commerce/app/components/nav/Navbar.tsx
@@ -3,8 +3,12 @@ import Container from "../Container";
 import Link from "next/link";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
-const Navbar = () => {
+const Navbar = async () => {
+
+    const user = await getCurrentUser();
+
     return (
         <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-md">
             <div className="py-4 border-b-[1px]">
@@ -14,7 +18,7 @@ const Navbar = () => {
                         <div className="hidden md:block">Search</div>
                         <div className="flex items-center gap-8 md:gap-12">
                             <CartCount />
-                            <UserMenu />
+                            <UserMenu user={user || null}/>
                         </div>
                     </div>
                 </Container>
