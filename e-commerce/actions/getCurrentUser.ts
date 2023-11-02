d182bd556b0dca4f24e1ee7d94130dc695e2d1a5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-02 22:25:59 +0200

    Update getCurrentUser.ts

--- e-commerce/actions/getCurrentUser.ts
+++ e-commerce/actions/getCurrentUser.ts
@@ -30,5 +30,7 @@ export async function getCurrentUser() {
             updatedAt: currentUser.updatedAt.toISOString(),
             emailVerified: currentUser.emailVerified?.toISOString() || null,
         };
-    } catch (error) {}
+    } catch (error) {
+        console.error(error);
+    }
 }
