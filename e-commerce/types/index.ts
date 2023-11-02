11d2970ab49187e5458d7edfe06e5e2dcc990336
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-02 16:35:26 +0200

    Create types for logged in user

---
+++ e-commerce/types/index.ts
@@ -0,0 +1,10 @@
+import { User } from "@prisma/client";
+
+export type LoggedInUser = Omit<
+    User,
+    "createdAt" | "updatedAt" | "emailVerified"
+> & {
+    createdAt: string;
+    updatedAt: string;
+    emailVerified: string | null;
+};
