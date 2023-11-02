c38da3d1cb89c9ff8c460cb797a6a0f50cf5ac3b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-02 11:22:24 +0200

    Register a user

--- e-commerce/app/register/RegisterForm.tsx
+++ e-commerce/app/register/RegisterForm.tsx
@@ -7,6 +7,10 @@ import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
     const [isLoading, setIsLoading] = React.useState<boolean>(false);
@@ -21,15 +25,51 @@ const RegisterForm = () => {
             password: "",
         },
     });
+    const router = useRouter();
 
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
+
+        axios
+            .post("/api/register", data)
+            .then(() => {
+                toast.success("Account created successfully");
+
+                signIn("credentials", {
+                    email: data.email,
+                    password: data.password,
+                    redirect: false,
+                }).then((callback) => {
+                    if (callback?.ok) {
+                        router.push("/cart");
+                        router.refresh();
+                        toast.success("Logged in successfully");
+                    }
+
+                    if (callback?.error) {
+                        toast.error("Error logging in");
+                        console.log(callback.error);
+                    }
+                });
+            })
+            .catch((err) => {
+                toast.error("Error creating account");
+                console.log(err);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
     };
 
     return (
         <>
             <Heading title="Sign Up" />
-            <Button label="Sign up with Google" outline onClick={() => {}} icon={AiOutlineGoogle} />
+            <Button
+                label="Sign up with Google"
+                outline
+                onClick={() => {}}
+                icon={AiOutlineGoogle}
+            />
             <hr className="bg-slate-300 w-full h-px" />
             <Input
                 id="name"
