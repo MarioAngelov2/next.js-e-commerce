173983f40722e83ab23c595bfd946cd4ccc74a05
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-02 11:23:43 +0200

    Backdrop component when nav menu is open

---
+++ e-commerce/app/components/nav/BackDrop.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface BackDropProps {
+    onClick: () => void;
+}
+
+const BackDrop: React.FC<BackDropProps> = ({ onClick }) => {
+    return (
+        <div
+            onClick={onClick}
+            className="z-20 bg-slate-200 opacity-50 w-screen h-screen fixed top-0 left-0
+            transition duration-300 ease-in-out"
+        ></div>
+    );
+};
+
+export default BackDrop;
