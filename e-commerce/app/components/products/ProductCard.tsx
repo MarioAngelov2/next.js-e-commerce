fc293bdd1e25865b2e1ed6491229aeb682400aea
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-03 22:33:43 +0200

    Update ProductCard.tsx

--- e-commerce/app/components/products/ProductCard.tsx
+++ e-commerce/app/components/products/ProductCard.tsx
@@ -23,7 +23,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
             className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50
         rounded-sm p-2 transition duration-300 hover:scale-105 text-center text-sm"
         >
-            <div className="flex flex-col items-center w-full gap-1">
+            <div className="flex flex-col items-center justify-between w-full max-h-[280px] md:max-h-[340px] h-screen gap-1">
                 <div className="aspect-square overflow-hidden relative w-full">
                     <Image
                         src={data.images[0].image}
@@ -32,12 +32,14 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
                         className="w-full h-full object-contain"
                     />
                 </div>
+                <div className="flex flex-col">
                 <div className="mt-4">{truncateText(data.name)}</div>
                 <div>
                     <Rating value={productRating} readOnly />
                 </div>
                 <div>{data.reviews.length} reviews</div>
                 <div className="font-semibold">{formatPrice(data.price)}</div>
+                </div>
             </div>
         </div>
     );
