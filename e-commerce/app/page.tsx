9464b087a93d3c089beabd45934894d84c63ddc1
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-03 22:33:45 +0200

    Update page.tsx

--- e-commerce/app/page.tsx
+++ e-commerce/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                 <div>
                     <HomeBanner />
                 </div>
-                <div className="flex flex-col gap-6 md:grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 md:gap-8">
+                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 md:gap-8">
                     {products.map((product: any) => (
                         <div key={product.id}>
                         <ProductCard data={product}/>
