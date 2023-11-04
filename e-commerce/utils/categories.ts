f184e7891c343c056bebfa2fa8fb44d9e687fb6a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-04 13:05:17 +0200

    Create category array

---
+++ e-commerce/utils/categories.ts
@@ -0,0 +1,33 @@
+import { AiFillPhone, AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+import { MdOutlineKeyboard, MdStorefront, MdTv, MdWatch } from "react-icons/md";
+
+export const categories = [
+    {
+        label: "All",
+        icon: MdStorefront,
+    },
+    {
+        label: "Phones",
+        icon: AiFillPhone,
+    },
+    {
+        label: "Laptops",
+        icon: AiOutlineLaptop,
+    },
+    {
+        label: "Desktops",
+        icon: AiOutlineDesktop,
+    },
+    {
+        label: "Watches",
+        icon: MdWatch,
+    },
+    {
+        label: "TV",
+        icon: MdTv,
+    },
+    {
+        label: "Accessories",
+        icon: MdOutlineKeyboard,
+    },
+];
