862878ce8b8c4b904298d2b7d4fbb16aeacde01d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-04 14:02:34 +0200

    Create color array

---
+++ e-commerce/utils/colors.ts
@@ -0,0 +1,42 @@
+export const colors = [
+    {
+        color: "Black",
+        colorCode: "#000000",
+        image: null,
+    },
+    {
+        color: "Silver",
+        colorCode: "#C0C0C0",
+        image: null,
+    },
+    {
+        color: "Gray",
+        colorCode: "#808080",
+        image: null,
+    },
+    {
+        color: "Red",
+        colorCode: "#FF0000",
+        image: null,
+    },
+    {
+        color: "Gold",
+        colorCode: "#FFD700",
+        image: null,
+    },
+    {
+        color: "Blue",
+        colorCode: "#0000FF",
+        image: null,
+    },
+    {
+        color: "Rose Gold",
+        colorCode: "#B76E79",
+        image: null,
+    },
+    {
+        color: "Space Grat",
+        colorCode: "#1D2951",
+        image: null,
+    },
+];
