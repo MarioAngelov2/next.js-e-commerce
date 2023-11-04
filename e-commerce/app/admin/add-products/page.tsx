2196d4a3ad2571f61252eebce93b73826a48048a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-04 11:37:42 +0200

    Wrap container with form wrap & add child enhancer

--- e-commerce/app/admin/add-products/page.tsx
+++ e-commerce/app/admin/add-products/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import Container from "@/app/components/Container";
+import FormWrap from "@/app/components/FormWrap";
+import React from "react";
+import AddProductForm from "./AddProductForm";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
 
-const AddProducts = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const AddProducts = async () => {
+    const user = await getCurrentUser();
 
-export default AddProducts
+    if (!user || user.role !== "ADMIN") {
+        return <NullData title="Access denied." />;
+    }
+
+    return (
+        <div className="p-8">
+            <Container>
+                <FormWrap>
+                    <AddProductForm />
+                </FormWrap>
+            </Container>
+        </div>
+    );
+};
+
+export default AddProducts;
