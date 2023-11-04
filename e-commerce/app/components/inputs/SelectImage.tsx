06feab2d758fa6a419bbfc0e198137f50ebeae69
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-04 14:55:27 +0200

    Create select image input

---
+++ e-commerce/app/components/inputs/SelectImage.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { ImageType } from "@/types/product";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+interface SelectImageProps {
+    item?: ImageType;
+    handleFileChange: (value: File) => void;
+}
+
+const SelectImage: React.FC<SelectImageProps> = ({
+    item,
+    handleFileChange,
+}) => {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        if (acceptedFiles.length > 0) {
+            handleFileChange(acceptedFiles[0]);
+        }
+    }, []);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: { "image/*": [".png", ".jpg", ".jpeg"] },
+    });
+
+    return (
+        <div
+            {...getRootProps()}
+            className="border-2 border-slate-400 p-2 border-dashed cursor-pointer 
+            text-sm font-normal text-slate-400 flex items-center justify-center"
+        >
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <p>Drop the Image here</p>
+            ) : (
+                <p>+ Upload Image</p>
+            )}
+        </div>
+    );
+};
+
+export default SelectImage;
