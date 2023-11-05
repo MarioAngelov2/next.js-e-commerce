138dae3614f08d23b116ba41b1bb8c0c10d0d386
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-05 11:34:44 +0200

    Create helper file & getting products

---
+++ e-commerce/actions/getProducts.ts
@@ -0,0 +1,49 @@
+import prisma from "@/libs/prismadb";
+
+export interface IProductParams {
+    category?: string | null;
+    searchTerm?: string | null;
+}
+
+export default async function getProducts(params: IProductParams) {
+    try {
+        const { category, searchTerm } = params;
+
+        let searchString = searchTerm;
+
+        if (!searchTerm) {
+            searchString = "";
+        }
+
+        let query: any = {};
+
+        if (category) {
+            query.category = category;
+        }
+
+        const products = await prisma.product.findMany({
+            where: {
+                ...query,
+                OR: [
+                    {
+                        name: { contain: searchString, mode: "insensitive" },
+                        description: {
+                            contain: searchString,
+                            mode: "insensitive",
+                        },
+                    },
+                ],
+            },
+            include: {
+                reviews: {
+                    include: { user: true },
+                    orderBy: { createdAt: "desc" },
+                },
+            },
+        });
+
+        return products;
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
