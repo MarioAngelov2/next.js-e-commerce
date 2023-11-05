fdbc90e06a1517a97074fde463303914c1fe9205
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-05 12:22:55 +0200

    Get products, user & display data table

--- e-commerce/app/admin/manage-products/page.tsx
+++ e-commerce/app/admin/manage-products/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import ManageProductsClient from "./ManageProductsClient";
+import Container from "@/app/components/Container";
+import getProducts from "@/actions/getProducts";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
 
-const ManageProducts = () => {
-    return <div></div>;
+const ManageProducts = async () => {
+    const products = await getProducts({ category: null });
+    const user = await getCurrentUser();
+
+    if (!user || user.role !== "ADMIN") {
+        return <NullData title="Access denied" />;
+    }
+
+    return (
+        <div>
+            <Container>
+                <ManageProductsClient products={products}/>
+            </Container>
+        </div>
+    );
 };
 
 export default ManageProducts;
