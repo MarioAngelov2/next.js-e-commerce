8343626d48e61ff7d2dc6e8228559d124e2602e3
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-06 10:44:14 +0200

    Create route for updating inStock status

--- e-commerce/app/api/product/route.ts
+++ e-commerce/app/api/product/route.ts
@@ -27,3 +27,23 @@ export async function POST(request: Request) {
 
     return NextResponse.json(product);
 }
+
+export async function PUT(request: Request) {
+    const user = await getCurrentUser();
+
+    if (!user || user.role !== "ADMIN") {
+        return NextResponse.error();
+    }
+
+    const body = await request.json();
+
+    const { id, inStock } = body;
+
+    const product = await prisma.product.update({
+        where: { id: id },
+        data: { inStock },
+    });
+
+    return NextResponse.json(product);
+}
+
