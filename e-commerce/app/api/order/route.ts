ca53e92c8fdd999789eeeafffeaf10f049228068
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-06 13:17:34 +0200

    Create route for handling delivery status

---
+++ e-commerce/app/api/order/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+
+export async function PUT(request: Request) {
+    const user = await getCurrentUser();
+
+    if (!user || user.role !== "ADMIN") {
+        return NextResponse.error();
+    }
+
+    const body = await request.json();
+
+    const { id, deliveryStatus } = body;
+
+    const order = await prisma.order.update({
+        where: { id: id },
+        data: { deliveryStatus },
+    });
+
+    return NextResponse.json(order);
+}
