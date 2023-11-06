2755a61b9bdf5b13ad1745f5c8989b26c5fe5879
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-06 16:13:02 +0200

    Fetch products. Shuffle products on home page

--- e-commerce/app/page.tsx
+++ e-commerce/app/page.tsx
@@ -1,11 +1,35 @@
 import Image from "next/image";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
-import { products } from "@/utils/products";
-import { truncateText } from "@/utils/truncateText";
 import ProductCard from "./components/products/ProductCard";
+import getProducts, { IProductParams } from "@/actions/getProducts";
+import NullData from "./components/NullData";
 
-export default function Home() {
+interface HomeProps {
+    searchParams: IProductParams;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+    const products = await getProducts(searchParams);
+
+    if (products.length === 0) {
+        return (
+            <NullData title="Something went wrong. Please refresh the page or try again later." />
+        );
+    }
+
+    // SHUFFLE PRODUCTS
+    const shuffleArrayProducts = (array: any) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+
+        return array;
+    };
+
+    const shuffleProducts = shuffleArrayProducts(products)
+
     return (
         <div className="p-8">
             <Container>
@@ -13,9 +37,9 @@ export default function Home() {
                     <HomeBanner />
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 md:gap-8">
-                    {products.map((product: any) => (
+                    {shuffleProducts.map((product: any) => (
                         <div key={product.id}>
-                        <ProductCard data={product}/>
+                            <ProductCard data={product} />
                         </div>
                     ))}
                 </div>
