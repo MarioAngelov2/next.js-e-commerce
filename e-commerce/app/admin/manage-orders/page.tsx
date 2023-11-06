ccf64ce30cd4e2124cb7fc0f1a6cba231d82e838
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-06 13:17:58 +0200

    Create manage order component

--- e-commerce/app/admin/manage-orders/page.tsx
+++ e-commerce/app/admin/manage-orders/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Container from "@/app/components/Container";
+import getProducts from "@/actions/getProducts";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+import ManageOrdersClient from "./ManageOrdersClient";
+import { getOrders } from "@/actions/getOrders";
 
-const ManageOrders = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const ManageProducts = async () => {
+    const orders = await getOrders();
+    const user = await getCurrentUser();
 
-export default ManageOrders
+    if (!user || user.role !== "ADMIN") {
+        return <NullData title="Access denied" />;
+    }
+
+    if (!orders) {
+        return <NullData title="No orders found" />;
+    }
+
+    return (
+        <div>
+            <Container>
+                <ManageOrdersClient orders={orders} />
+            </Container>
+        </div>
+    );
+};
+
+export default ManageProducts;
