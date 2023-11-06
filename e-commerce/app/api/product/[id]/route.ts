7956aaf9c087e6f2924088f3a84758f6d232ddea
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-06 10:44:24 +0200

    Create route for deleting a product

---
+++ e-commerce/app/api/product/[id]/route.ts
@@ -0,0 +1,20 @@
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import prisma from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    const user = await getCurrentUser();
+
+    if (!user || user.role !== "ADMIN") {
+        return NextResponse.error();
+    }
+
+    const product = await prisma.product.delete({
+        where: { id: params.id },
+    });
+
+    return NextResponse.json(product);
+}
