de4979b76786f68b6eda0a6f06e55b437d295a86
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-07 14:58:02 +0200

    Create search bar component

---
+++ e-commerce/app/components/SearchBar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import queryString from "query-string";
+import React from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+const SearchBar = () => {
+    const router = useRouter();
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            searchTerm: "",
+        },
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        if (!data.searchTerm) return router.push("/");
+
+        const url = queryString.stringifyUrl(
+            {
+                url: "/",
+                query: { searchTerm: data.searchTerm },
+            },
+            { skipNull: true }
+        );
+
+        router.push(url);
+        reset();
+    };
+
+    return (
+        <div className="hidden md:flex items-center gap-2">
+            <input
+                {...register("searchTerm")}
+                autoComplete="off"
+                type="text"
+                placeholder="Explore products"
+                className="p-2 border rounded-md w-80 outline-none 
+                hover:shadow-md focus:shadow-md shadow-gray-100 transition 
+                duration-200 ease-in-out"
+            />
+            <button
+                onClick={handleSubmit(onSubmit)}
+                className="bg-slate-700 text-white p-2 rounded-md 
+                hover:opacity-80 transition duration-200 ease-in-out"
+            >
+                Search
+            </button>
+        </div>
+    );
+};
+
+export default SearchBar;
