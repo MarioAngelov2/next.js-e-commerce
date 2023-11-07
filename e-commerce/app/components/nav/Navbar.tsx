bdcb46ff4128fd2d973c301b924febeeee65c7d0
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-07 14:57:53 +0200

    Add search bar component

--- e-commerce/app/components/nav/Navbar.tsx
+++ e-commerce/app/components/nav/Navbar.tsx
@@ -5,6 +5,7 @@ import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import CategoriesBar from "./CategoriesBar";
+import SearchBar from "../SearchBar";
 
 const Navbar = async () => {
 
@@ -16,7 +17,7 @@ const Navbar = async () => {
                 <Container>
                     <div className="flex items-center justify-between gap-3 md-gap-0">
                         <Link href="/">E-Shop</Link>
-                        <div className="hidden md:block">Search</div>
+                        <SearchBar />
                         <div className="flex items-center gap-8 md:gap-7">
                             <CartCount />
                             <UserMenu user={user || null}/>
