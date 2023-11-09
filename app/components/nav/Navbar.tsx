124daca053eafdf9d3a0a06979c342930726c86a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-09 11:35:00 +0200

    Update Navbar.tsx

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -8,19 +8,23 @@ import CategoriesBar from "./CategoriesBar";
 import SearchBar from "../SearchBar";
 
 const Navbar = async () => {
-
     const user = await getCurrentUser();
 
     return (
         <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-md">
             <div className="py-4 border-b-[1px]">
                 <Container>
-                    <div className="flex items-center justify-between gap-3 md-gap-0">
-                        <Link href="/" className="font-bold text-xl md:text-2xl">E-SHOP</Link>
+                    <div className="flex flex-col md:flex-row items-center md:justify-between gap-3 md-gap-0">
+                        <Link
+                            href="/"
+                            className="font-bold text-xl md:text-2xl"
+                        >
+                            E-SHOP
+                        </Link>
                         <SearchBar />
                         <div className="flex items-center gap-8 md:gap-7">
                             <CartCount />
-                            <UserMenu user={user || null}/>
+                            <UserMenu user={user || null} />
                         </div>
                     </div>
                 </Container>
